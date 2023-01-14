5747163e8b7fb85ba040ec92fb1ec6cbaff3471f
Maya Lynn <mayalynncohen@gmail.com>
2023-01-13 17:27:01 -0800

    put game in function so it appears only when called

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,109 +12,134 @@ var divC3 = document.getElementById("c3")
 yesEl.style.display = "none"
 noEl.style.display = "none"
 
-// hangman game elements
-var wordPool = ["elves", "human", "dwarf", "castle", "forest", "dragon", "princess", "witch", "troll"]
-var randomWord;
-var maskedWord = [];
-var timeLeft = 60;
-var wordsFound = 0;
+function playHangman() {
+    // hangman game elements
+    var wordPool = ["elves", "human", "dwarf", "castle", "forest", "dragon", "princess", "witch", "troll"]
+    var randomWord;
+    var maskedWord = [];
+    var timeLeft = 30;
+    var wordsFound = 0;
 
-// var miniGameContainer = document.createElement("div")
-// miniGameContainer.setAttribute("id", "miniGame")
+    // generating hangman game element
+    var miniGameContainer = document.createElement("div")
+    miniGameContainer.setAttribute("id", "miniGame")
 
-// var miniGameTitle = document.createElement("p")
-// miniGameTitle.textContent = "The Hangman Game"
+    var miniGameTitle = document.createElement("p")
+    miniGameTitle.textContent = "The Hangman Game"
 
-// var wordFoundEl = document.createElement("p")
-// wordFoundEl.setAttribute("id", "wordsFound")
+    var wordEl = document.createElement("p")
+    wordEl.setAttribute("id", "wordToFind")
 
-var startMiniGameBtn = document.getElementById("hangmanStartBtn")
-// startMiniGameBtn.setAttribute("")
+    var startMiniGameBtn = document.createElement("button")
+    startMiniGameBtn.textContent = "Start!"
+    startMiniGameBtn.setAttribute("id", "hangmanStartBtn")
 
-// miniGameContainer.appendChild(miniGameTitle)
-// miniGameTitle.append(wordFoundEl)
+    miniGameContainer.appendChild(miniGameTitle)
+    miniGameContainer.appendChild(wordEl)
+    miniGameContainer.appendChild(startMiniGameBtn)
 
-var wordEl = document.getElementById("wordToFind")
-var wordFoundEl = document.getElementById("wordsFound")
-var timeLeftEl = document.getElementById("timeLeft")
+    divC3.appendChild(miniGameContainer)
 
-dialogueNextBnt.addEventListener("click", function() {
-    
-})
+    // generating hangman words found element
+    var wordsFoundEl = document.createElement("p")
+    wordsFoundEl.setAttribute("id", "wordsFound")
+    wordsFoundEl.textContent = "0/5 words"
 
+    // var wordsFoundContainer = document.createElement("div")
+    // wordsFoundContainer.setAttribubte("id", "wordsFoundContainer")
+    // wordsFoundContainer.appendChild(wordsFoundEl)
+    divA1.appendChild(wordsFoundEl)
 
+    // generating hangman timeer element
+    var timeLeftContainer = document.createElement("div")
+    timeLeftContainer.setAttribute("id", "timer")
+    var timeLeftText = document.createElement("p")
+    timeLeftText.textContent = "time left"
+    var timeLeftEl = document.createElement("p")
+    timeLeftEl.setAttribute("id", "timeLeft")
+    timeLeftEl.textContent = timeLeft + " seconds"
 
-startMiniGameBtn.addEventListener("click", function() {
-    mainGameBackBtn.setAttribute("class", "nes-btn is-disabled");
-    countdown();
-    randomizer();
-    wordEl.textContent = maskedWord.join(" ");
-    startMiniGameBtn.style.display = "none"
-})
+    timeLeftContainer.appendChild(timeLeftText)
+    timeLeftContainer.appendChild(timeLeftEl)
 
-function randomizer() {
-    maskedWord = [];
-    randomWord = wordPool[Math.floor(Math.random() * wordPool.length)];
-    for(var i = 0; i < randomWord.length; i++) {
-        maskedWord.push("_");
+    divA5.appendChild(timeLeftContainer)
+
+    dialogueNextBnt.addEventListener("click", function () {
+
+    })
+
+    startMiniGameBtn.addEventListener("click", function () {
+        mainGameBackBtn.setAttribute("class", "nes-btn is-disabled");
+        countdown();
+        randomizer();
         wordEl.textContent = maskedWord.join(" ");
-    }
-};
+        startMiniGameBtn.style.display = "none"
+    })
 
-var key;
-
-function letterInWord() {
-    for(var i = 0; i < randomWord.length; i++) {
-        if(randomWord[i] === key) {
-            maskedWord.splice([i], 1, randomWord[i]);
+    function randomizer() {
+        maskedWord = [];
+        randomWord = wordPool[Math.floor(Math.random() * wordPool.length)];
+        for (var i = 0; i < randomWord.length; i++) {
+            maskedWord.push("_");
             wordEl.textContent = maskedWord.join(" ");
-            if(!maskedWord.includes("_")){
-                wordsFound++
-                wordFoundEl.textContent = wordsFound + "/5 words";
-                setTimeout(randomizer(), 1000)
-                return;
-            } 
         }
-    }
-}
+    };
 
-document.addEventListener("keydown", keydownAction);
+    var key;
 
-function keydownAction(event) {
-    key = event.key.toLowerCase();
-    return letterInWord();
-}
-
-function countdown() {
-    timeLeft = 30;
-    timerInterval = setInterval(function() {
-        timeLeft--;
-        timeLeftEl.textContent = timeLeft + " seconds"
-        if(timeLeft <= 0) {
-            clearInterval(timerInterval);
-            randomWord = '';
-            maskedWord = [];
-            wordEl.textContent = "Time is up!";
-            if(wordsFound < 5) {
-                wordEl.textContent = "Game over!";
-                dialogueTextEl.textContent = "You did not make it in time! you only found " + wordsFound + " out of 5 words. You lost 1 life... Would you like to try again?"
-                mainGameBackBtn.setAttribute("class", "nes-btn");
-                yesEl.style.display = "block"
-                noEl.style.display = "block"
-            } else if(wordsFound === 5) {
-                dialogueTextEl.textContent = "You have successfully found all 5 words! Congratulations! You gained 500 XP";
-                yesEl.style.display = "none"
-                noEl.style.display = "none"
-                mainGameContinueBtn.setAttribute("class", "nes-btn");
-            } else {
-                var addXP = ((wordsFound-5)*10)+500;
-                var extraWords = (wordsFound-5);
-                dialogueTextEl.textContent = "You have successfully found all 5 words and " + extraWords + " extra words! Congratulations! You gained " + addXP + "XP."
-                yesEl.style.display = "none"
-                noEl.style.display = "none"
-                mainGameContinueBtn.setAttribute("class", "nes-btn");
+    function letterInWord() {
+        for (var i = 0; i < randomWord.length; i++) {
+            if (randomWord[i] === key) {
+                maskedWord.splice([i], 1, randomWord[i]);
+                wordEl.textContent = maskedWord.join(" ");
+                if (!maskedWord.includes("_")) {
+                    wordsFound++
+                    wordsFoundEl.textContent = wordsFound + "/5 words";
+                    setTimeout(randomizer(), 1000)
+                    return;
+                }
             }
         }
-    }, 1000)
+    }
+
+    document.addEventListener("keydown", keydownAction);
+
+    function keydownAction(event) {
+        key = event.key.toLowerCase();
+        return letterInWord();
+    }
+
+    function countdown() {
+        timeLeft = 30;
+        timerInterval = setInterval(function () {
+            timeLeft--;
+            timeLeftEl.textContent = timeLeft + " seconds"
+            if (timeLeft <= 0) {
+                clearInterval(timerInterval);
+                randomWord = '';
+                maskedWord = [];
+                wordEl.textContent = "Time is up!";
+                if (wordsFound < 5) {
+                    wordEl.textContent = "Game over!";
+                    dialogueTextEl.textContent = "You did not make it in time! you only found " + wordsFound + " out of 5 words. You lost 1 life... Would you like to try again?"
+                    mainGameBackBtn.setAttribute("class", "nes-btn");
+                    yesEl.style.display = "block"
+                    noEl.style.display = "block"
+                } else if (wordsFound === 5) {
+                    dialogueTextEl.textContent = "You have successfully found all 5 words! Congratulations! You gained 500 XP";
+                    yesEl.style.display = "none"
+                    noEl.style.display = "none"
+                    mainGameContinueBtn.setAttribute("class", "nes-btn");
+                } else {
+                    var addXP = ((wordsFound - 5) * 10) + 500;
+                    var extraWords = (wordsFound - 5);
+                    dialogueTextEl.textContent = "You have successfully found all 5 words and " + extraWords + " extra words! Congratulations! You gained " + addXP + "XP."
+                    yesEl.style.display = "none"
+                    noEl.style.display = "none"
+                    mainGameContinueBtn.setAttribute("class", "nes-btn");
+                }
+            }
+        }, 1000)
+    }
 }
 
