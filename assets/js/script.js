522e7f6c383068e90a08092c7068f21e43ec8fac
Maya Lynn <mayalynncohen@gmail.com>
2023-01-13 16:17:14 -0800

    mainGame script better layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,48 @@
 // main game elements
 var mainGameBackBtn = document.getElementById("main-back-btn")
+var mainGameContinueBtn = document.getElementById("main-continue-btn")
+var dialogueTextEl = document.getElementById("dialogueText")
+var yesEl = document.getElementById("yes")
+var noEl = document.getElementById("no")
+var dialogueNextBnt = document.getElementById("dialogueBtn")
+var divA1 = document.getElementById("a1")
+var divA5 = document.getElementById("a5")
+var divC3 = document.getElementById("c3")
 
-// hangman elements
+yesEl.style.display = "none"
+noEl.style.display = "none"
+
+// hangman game elements
 var wordPool = ["elves", "human", "dwarf", "castle", "forest", "dragon", "princess", "witch", "troll"]
 var randomWord;
 var maskedWord = [];
 var timeLeft = 60;
 var wordsFound = 0;
 
+// var miniGameContainer = document.createElement("div")
+// miniGameContainer.setAttribute("id", "miniGame")
+
+// var miniGameTitle = document.createElement("p")
+// miniGameTitle.textContent = "The Hangman Game"
+
+// var wordFoundEl = document.createElement("p")
+// wordFoundEl.setAttribute("id", "wordsFound")
+
 var startMiniGameBtn = document.getElementById("hangmanStartBtn")
+// startMiniGameBtn.setAttribute("")
+
+// miniGameContainer.appendChild(miniGameTitle)
+// miniGameTitle.append(wordFoundEl)
+
 var wordEl = document.getElementById("wordToFind")
 var wordFoundEl = document.getElementById("wordsFound")
+var timeLeftEl = document.getElementById("timeLeft")
 
-document.getElementById("yes").style.display = "none"
-document.getElementById("no").style.display = "none"
+dialogueNextBnt.addEventListener("click", function() {
+    
+})
+
+
 
 startMiniGameBtn.addEventListener("click", function() {
     mainGameBackBtn.setAttribute("class", "nes-btn is-disabled");
@@ -60,7 +89,7 @@ function countdown() {
     timeLeft = 30;
     timerInterval = setInterval(function() {
         timeLeft--;
-        document.getElementById("timeLeft").textContent = timeLeft + " seconds"
+        timeLeftEl.textContent = timeLeft + " seconds"
         if(timeLeft <= 0) {
             clearInterval(timerInterval);
             randomWord = '';
@@ -68,29 +97,24 @@ function countdown() {
             wordEl.textContent = "Time is up!";
             if(wordsFound < 5) {
                 wordEl.textContent = "Game over!";
-                document.getElementById("dialogueText").textContent = "You did not make it in time! you only found " + wordsFound + " out of 5 words. You lost 1 life... Would you like to try again?"
-                document.getElementById("main-back-btn").setAttribute("class", "nes-btn");
-                document.getElementById("yes").style.display = "block"
-                document.getElementById("no").style.display = "block"
+                dialogueTextEl.textContent = "You did not make it in time! you only found " + wordsFound + " out of 5 words. You lost 1 life... Would you like to try again?"
+                mainGameBackBtn.setAttribute("class", "nes-btn");
+                yesEl.style.display = "block"
+                noEl.style.display = "block"
             } else if(wordsFound === 5) {
-                document.getElementById("dialogueText").textContent = "You have successfully found all 5 words! Congratulations! You gained 500 XP";
-                document.getElementById("yes").style.display = "none"
-                document.getElementById("no").style.display = "none"
-                document.getElementById("main-continue-btn").setAttribute("class", "nes-btn");
+                dialogueTextEl.textContent = "You have successfully found all 5 words! Congratulations! You gained 500 XP";
+                yesEl.style.display = "none"
+                noEl.style.display = "none"
+                mainGameContinueBtn.setAttribute("class", "nes-btn");
             } else {
                 var addXP = ((wordsFound-5)*10)+500;
                 var extraWords = (wordsFound-5);
-                document.getElementById("dialogueText").textContent = "You have successfully found all 5 words and " + extraWords + " extra words! Congratulations! You gained " + addXP + "XP."
-                document.getElementById("yes").style.display = "none"
-                document.getElementById("no").style.display = "none"
-                document.getElementById("main-continue-btn").setAttribute("class", "nes-btn");
+                dialogueTextEl.textContent = "You have successfully found all 5 words and " + extraWords + " extra words! Congratulations! You gained " + addXP + "XP."
+                yesEl.style.display = "none"
+                noEl.style.display = "none"
+                mainGameContinueBtn.setAttribute("class", "nes-btn");
             }
         }
     }, 1000)
 }
 
-var dialogueNextBnt = document.getElementById("dialogueBtn")
-
-dialogueNextBnt.addEventListener("click", function() {
-    
-})
